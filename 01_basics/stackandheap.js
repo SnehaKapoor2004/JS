a00fb28a1016272d93bf7a25434fbb1bf27a3c66
SnehaKapoor2004 <157450175+SnehaKapoor2004@users.noreply.github.com>
2025-07-09 12:59:11 +0000

    stackandheap.js

---
+++ 01_basics/stackandheap.js
@@ -0,0 +1,22 @@
+// There are two types of memory stack and heap
+
+// stack use ime (primitive) , heap(non-primitive)
+
+let myYoutube="samcom"
+let anohername=myYoutube;
+anohername="sam"
+//console.log(myYoutube)
+//console.log(anohername)
+
+let user={
+    email:"sam@g,ail.com",
+    upi:"user@ybl"
+}
+let user2=user
+user2={
+    email:"sammi@gmail.com",
+    name:"sam",
+    password:123
+}
+console.log(user)
+console.log(user2)
